149053b06592b03fd23b65f7fb8f11b28e67c8e8
Jared Teems <jared.teems@adjenti.com>
2014-05-12 19:49:15 -0400

    Added new commands.

---
+++ backend.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def is_installed(path):
+    """Check if a database has already been initialized"""
+    return False # Stub code
+
+def init_db(path):
+    pass # Stub code
+
+def reset(day):
+    pass
+
+def get_config(key):
+    return 1
--- pedway.py
+++ pedway.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python3
 import argparse
 import findTime
+import backend
 
 def add(args):
-    specDate = findTime.findDay(a.day)
+    specDate = findTime.findDay(args.day)
     print("Recording {steps:,} steps on {date}".format(steps = a.steps, date=specDate))
 
 def update(args):
-    specDate = findTime.findDay(a.day)
-    print("Changing {date} to {steps:,} steps".format(steps = a.steps, date=specDate))
+    specDate = findTime.findDay(args.day)
+    print("Changing {date} to {steps:,} steps walked".format(steps = a.steps, date=specDate))
+
+def reset(args):
+    specDate = findTime.findDay(args.day)
+    answer = input("Are you sure you want to reset {day}? [no] ".format(day=specDate))
+    if answer == "yes" or answer == "y":
+        print("Resetting walked steps on {day}.".format(day=specDate))
+        backend.reset(specDate)
+    else:
+        print("Okay then. I won't reset {day}".format(day=specDate))
+
+def install(args):
+    print("Checking for install at {path}".format(path=a.path))
+    if not backend.is_installed(a.path):
+        print("Setting up new database")
+        backend.init_db(args.path)
+    else:
+        print("The database is already setup")
+
+def config(args):
+    parameters = args.params
+    if len(parameters) == 0:
+        print("Here's your current configuration:")
+    elif len(parameters) == 1:
+        currentValue = backend.get_config(parameters[0])
+        print("'{key}' is currently '{value}'".format(key=parameters[0],value=currentValue))
+    else:
+        print("Setting {key} to {value}".format(key=parameters[0],value=parameters[1]))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="pedway")
@@ -25,5 +53,19 @@ if __name__ == "__main__":
     updateParser.add_argument("steps",type=int)
     updateParser.set_defaults(func=update)
 
+    resetParser = subParsers.add_parser("reset", parents=[parentParser])
+    resetParser.set_defaults(func=reset)
+
+    installParser = subParsers.add_parser("install", parents=[parentParser])
+    installParser.add_argument("--path", "-p", default="~/.pedway")
+    installParser.set_defaults(func=install)
+
+    configParser = subParsers.add_parser("config", parents=[parentParser])
+    configParser.add_argument("params", nargs="*", default=[])
+    configParser.set_defaults(func=config)
+
     a = parser.parse_args()
-    a.func(a)
+    if hasattr(a,"func"):
+        a.func(a)
+    else:
+        parser.print_help()
